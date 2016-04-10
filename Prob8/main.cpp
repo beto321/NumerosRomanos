1bdf16cebf90e2025a7b242a6bc5190d82eb5545
IvoPc <ivoalabe@gmail.com>
2016-04-09 23:00:24 -0400

    mitad del ejercicio

---
+++ Prob8/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, i;
+    int Vn[13]= {1000, 900, 500, 400, 100, 90, 40, 10, 9, 5, 4,1};
+    char *Vc[13]={"m","cm","cd","c","xc","l","xl","x","ix","v","iv","i"};
+    while(1){
+        scanf("%d",&x);
+        if(x==0)
+            break;
+        printf("%-4d  ",x);
+        i=0;
+        while(x>0){
+            if(x>Vn[i]){
+
+            }
+        }
+    }
+
+
+        return 0;
+}
