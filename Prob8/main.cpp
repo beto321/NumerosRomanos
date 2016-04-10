955d61ad4168c778cdbf8fb5ac90d0d11ee62d6a
IvoPc <ivoalabe@gmail.com>
2016-04-09 23:16:16 -0400

    si funciona!!

--- Prob8/main.cpp
+++ Prob8/main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main()
 {
     int x, i;
-    int Vn[13]= {1000, 900, 500, 400, 100, 90, 40, 10, 9, 5, 4,1};
-    char *Vc[13]={"m","cm","cd","c","xc","l","xl","x","ix","v","iv","i"};
+    int Vn[13]= {1000, 900, 500, 400, 100, 90, 50,40, 10, 9, 5, 4,1};
+    char *Vc[13]={"m","cm","d","cd","c","xc","l","xl","x","ix","v","iv","i"};
     while(1){
         scanf("%d",&x);
         if(x==0)
@@ -14,7 +14,7 @@ int main()
         printf("%-4d  ",x);
         i=0;
         while(x>0){
-            if(x>Vn[i]){
+            if(x>=Vn[i]){
                 printf("%s",Vc[i]);
                 x=x-Vn[i];
 
